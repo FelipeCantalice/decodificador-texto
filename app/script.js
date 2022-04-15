10e6825384cf33c4e4db885975959592489b6e72
Felipe Cantalice <felipep31a@gmail.com>
2022-04-15 11:25:16 -0400

    validação de texto do input - não permite letras maisculas e especiais

--- app/script.js
+++ app/script.js
@@ -6,6 +6,7 @@ const CRIPT_WORDS = {
     'u': 'ufat',
 };
 
+// algoritmo de criptografia
 function criptografar(texto = new String()) {
     let texto_criptografado = new String();
 
@@ -19,16 +20,28 @@ function criptografar(texto = new String()) {
     return texto_criptografado;
 }
 
-const teste = 'gato'
-console.log(criptografar(teste))
-
-
-
 const button_cript = document.querySelector('#btn-codificar');
 
 button_cript.addEventListener('click', function () {
     const texto = document.querySelector('#texto').value;
+
+    // validar texto digirado
+    const caracter_especiais = /[^a-zA-Z0-9]/g;
+    const caracter_uppercases = /[A-Z]/g;
+
+    if (texto.match(caracter_especiais)) {
+        alert('Por favor!\nDigite apenas letras e números.');
+        return;
+    }
+    if (texto.match(caracter_uppercases) != null) {
+        alert('Por favor!\nDigite apenas letras minúsculas.');
+        return 
+    }
+
+
+    // criptografar texto
     const texto_criptografado = criptografar(texto);
+    // atualizar DOM com o texto criptografado
     document.querySelector('.info-section').classList.remove('empty');
     document.querySelector('.empty-message').classList.add('hide');
     document.querySelector('#info-text').innerHTML = texto_criptografado;
